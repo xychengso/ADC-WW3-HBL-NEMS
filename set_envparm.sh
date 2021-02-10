a773bdedf5ce5315d1884f5e29781dc24891e691
uri-xuanyu.chen <xychen@my.uri.edu>
2021-02-10 16:11:54 -0500

    minor update

--- set_envparm.sh
+++ set_envparm.sh
@@ -4,7 +4,7 @@ export ROOTDIR=/path-to-your/ADC-WW3-HBL-NEMS
 export LIBDIR=/projects/ees/dhs-crc/xychen
 export METIS_PATH=/home/xychen/localmodules/parmetis
 export SZIPHOME=/home/xychen/localmodules/szip/2.1/lib
-export ESMFMKFILE=/projects/ees/dhs-crc/xychen/ESMFlib_openmpi/lib/libg/Linux.intel.64.openmpi.default/esmf.mk
+export ESMFMKFILE=/projects/ees/dhs-crc/xychen/ESMFlib_openmpi_xyc/lib/libg/Linux.intel.64.openmpi.default/esmf.mk
 #export ESMFMKFILE=/projects/ees/dhs-crc/xychen/ESMFlib/lib/libg/Linux.intel.64.mvapich2.default/esmf.mk
 #export ESMFMKFILE=/home/mjisan/regesm/reg/new/esmf-5.2.0r/install_dir/lib/libO/Linux.intel.64.intelmpi.default
 
